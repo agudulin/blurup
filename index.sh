04b0c8d7de7da1edf9af020cf53632a48de21d65
Alexander Gudulin <alexandr.gudulin@gmail.com>
2017-04-07 16:11:57 +0200

    :zap: refactor a bit

--- index.sh
+++ index.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 svgify () {
-  local image=$1
-  local width=`identify -format '%w' $image`
-  local height=`identify -format '%h' $image`
-  local extension=`identify -format '%e' $image`
-  local data="$(
-    convert $image -resize 40x22 -quality 100 $extension:-\
+  local image, width, height, extension, data
+
+  image=$1
+  width=$(identify -format '%w' "$image")
+  height=$(identify -format '%h' "$image")
+  extension=$(identify -format '%e' "$image")
+  data="$(
+    convert "$image" -resize 40x22 -quality 100 "$extension":-\
       | openssl base64\
       | tr -d \\n
     )"
@@ -17,9 +19,9 @@ svgify () {
   " index.svg
 }
 
-if [ -z $1 ] || [ ! -f $1 ] ; then
+if [ -z "$1" ] || [ ! -f "$1" ] ; then
   echo "error. i need an image."
   exit 1
 else
-  svgify $1
+  svgify "$1"
 fi
